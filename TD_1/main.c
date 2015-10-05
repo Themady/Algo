aa203837d454b8f2cce470c8f93e0cc3b76c87d1
Themady <Themady@gmx.fr>
2015-10-06 00:50:10 +0200

    test
    test

---
+++ TD_1/main.c
@@ -0,0 +1,16 @@
+/*
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{int a =12 
+printf ("la valeur de a%d\n,a") }
+*/
+#include "header.h"
+
+void main()
+{
+	//exo1();
+	exo2();
+	//exo3();
+	system("pause");
+}
