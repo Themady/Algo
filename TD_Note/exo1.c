0b57a190d06e268e65c9dabf6d579d60c399efd9
Themady <Themady@gmx.fr>
2015-10-16 14:49:57 +0200

    TD
    TD

---
+++ TD_Note/exo1.c
@@ -0,0 +1,6 @@
+int tiers(int nbr)//la fonction doit être de type int pour envoyer une valeur de sortie
+{
+	float res = 0; // un seul = pour attribuer la valeur
+	res = nbr / 3;
+	return res;//Ajouter le ; et changer resultat en res pour en renvoyer la valeur
+}
